9c90e6d5161ba806c8d97b9a7a45018eaa82d507
afomic <afomic1@gmail.com>
2019-02-05 23:32:14 +0100

    added customer home activity

---
+++ app/src/main/java/afomic/com/camfood/ui/customerHome/CustomerHomeActivity.java
@@ -0,0 +1,15 @@
+package afomic.com.camfood.ui.customerHome;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import afomic.com.camfood.R;
+
+public class CustomerHomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_customer_home);
+    }
+}
