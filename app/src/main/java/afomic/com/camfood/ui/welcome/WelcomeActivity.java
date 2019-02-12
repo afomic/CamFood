8f7d058e24f4a17092db5090d325ebae8d7673c3
afomic <afomic1@gmail.com>
2019-02-12 23:33:52 +0100

    added login span

--- app/src/main/java/afomic/com/camfood/ui/welcome/WelcomeActivity.java
+++ app/src/main/java/afomic/com/camfood/ui/welcome/WelcomeActivity.java
@@ -2,16 +2,25 @@ package afomic.com.camfood.ui.welcome;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
+import android.text.SpannableString;
+import android.text.method.LinkMovementMethod;
+import android.text.style.ClickableSpan;
+import android.view.View;
+import android.widget.TextView;
 
 import afomic.com.camfood.R;
 import afomic.com.camfood.ui.home.HomeActivity;
 import afomic.com.camfood.ui.login.LoginActivity;
+import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 
 public class WelcomeActivity extends AppCompatActivity implements WelcomeView {
     private WelcomePresenter mWelcomePresenter;
+    @BindView(R.id.tv_login_span)
+    TextView loginSpanTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,10 +28,21 @@ public class WelcomeActivity extends AppCompatActivity implements WelcomeView {
         setContentView(R.layout.activity_welcome);
         ButterKnife.bind(this);
         mWelcomePresenter = new WelcomePresenter(this);
+        mWelcomePresenter.loadView();
     }
 
     @Override
     public void setup() {
+        String text = loginSpanTextView.getText().toString();
+        SpannableString spannableString = new SpannableString(text);
+        spannableString.setSpan(new ClickableSpan() {
+            @Override
+            public void onClick(@NonNull View view) {
+                mWelcomePresenter.handleLoginSpan();
+            }
+        }, 0, text.length(), 0);
+        loginSpanTextView.setMovementMethod(new LinkMovementMethod());
+        loginSpanTextView.setText(spannableString);
 
     }
 
--- app/src/main/java/afomic/com/camfood/ui/welcome/WelcomePresenter.java
+++ app/src/main/java/afomic/com/camfood/ui/welcome/WelcomePresenter.java
@@ -1,6 +1,5 @@
 package afomic.com.camfood.ui.welcome;
 
-import afomic.com.camfood.data.SharedPreferenceHelper;
 import afomic.com.camfood.ui.base.BasePresenter;
 
 public class WelcomePresenter extends BasePresenter<WelcomeView> {
@@ -12,14 +11,21 @@ public class WelcomePresenter extends BasePresenter<WelcomeView> {
     public void loadView() {
 
     }
-    public void handleSkipClick(){
+
+    public void handleSkipClick() {
         view.showHomeView();
     }
-    public void handleCustomerRegistrationClick(){
+
+    public void handleCustomerRegistrationClick() {
         view.showCustomerRegistrationView();
     }
-    public void handleRestaurantRegistrationClick(){
+
+    public void handleRestaurantRegistrationClick() {
         view.showRestaurantRegistrationView();
+    }
+
+    public void handleLoginSpan() {
+        view.showLoginView();
     }
 
 }
