f286cf00627f8aeb4fe9df9fc7931690fc75c28b
afomic <mafolabi@teamapt.com>
2019-03-26 07:42:20 +0100

    deduct fund for food ord

--- app/src/main/java/afomic/com/camfood/ui/orderCheckout/OrderCheckoutPresenter.java
+++ app/src/main/java/afomic/com/camfood/ui/orderCheckout/OrderCheckoutPresenter.java
@@ -3,6 +3,7 @@ package afomic.com.camfood.ui.orderCheckout;
 import java.util.List;
 
 import afomic.com.camfood.Constants;
+import afomic.com.camfood.R;
 import afomic.com.camfood.data.AuthManager;
 import afomic.com.camfood.data.DataSource;
 import afomic.com.camfood.data.ResponseCallback;
@@ -63,14 +64,17 @@ public class OrderCheckoutPresenter extends BasePresenter<OrderCheckoutView> {
             return;
         }
         view.showProgressView();
-        mOrder.addStatus(new OrderStatus(Constants.ORDER_STATUS_CREATED, System.currentTimeMillis()));
         mAuthManager.getCurrentUser(new AuthManager.AuthCallback() {
             @Override
             public void onSuccess(User user) {
-                mOrder.setUserId(user.id);
-                mOrder.setUserPhoneNumber(user.phonNumber);
-                mOrder.setUserName(user.name);
-                saveOrder();
+                int amount = mOrder.getAmount();
+                if (user.accountBalance > amount) {
+                    createOrder();
+
+                } else {
+                    view.hideProgressView();
+                    view.showMessage(R.string.insufficient_balance);
+                }
             }
 
             @Override
@@ -82,11 +86,43 @@ public class OrderCheckoutPresenter extends BasePresenter<OrderCheckoutView> {
 
     }
 
-    public void saveOrder() {
+    private void createOrder() {
+        mOrder.addStatus(new OrderStatus(Constants.ORDER_STATUS_CREATED, System.currentTimeMillis()));
+        mAuthManager.getCurrentUser(new AuthManager.AuthCallback() {
+            @Override
+            public void onSuccess(User user) {
+                mOrder.setUserId(user.id);
+                mOrder.setUserPhoneNumber(user.phonNumber);
+                mOrder.setUserName(user.name);
+                user.accountBalance+=mOrder.getAmount();
+                mAuthManager.updateUser(user, new AuthManager.AuthCallback() {
+                    @Override
+                    public void onSuccess(User user) {
+                        saveOrder();
+                    }
+
+                    @Override
+                    public void onError(String reason) {
+                        view.hideProgressView();
+                        view.showMessage(reason);
+                    }
+                });
+
+            }
+
+            @Override
+            public void onError(String reason) {
+
+            }
+        });
+    }
+
+    private void saveOrder() {
         mOrderDataSource.save(mOrder, new ResponseCallback() {
             @Override
             public void onSuccess() {
                 view.hideProgressView();
+                view.showMessage("Order Successfully created");
                 view.showHome();
             }
 
@@ -97,5 +133,6 @@ public class OrderCheckoutPresenter extends BasePresenter<OrderCheckoutView> {
             }
         });
     }
+
 
 }
